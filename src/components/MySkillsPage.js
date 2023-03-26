8b8c2b386fbc090ad247b37651d9eea33eab49de
hakanonay <dr.hakanonay@gamil.com>
2023-03-26 01:06:06 +0100

    formatter added

--- src/components/MySkillsPage.js
+++ src/components/MySkillsPage.js
@@ -1,136 +1,139 @@
-import React from 'react'
-import styled, { ThemeProvider } from 'styled-components'
-import { lightTheme } from './Themes';
-import { Design, Develope } from './AllSvgs';
+import React from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { lightTheme } from "./Themes";
+import { Design, Develope } from "./AllSvgs";
 
-
-import LogoComponent from '../subComponents/LogoComponent';
-import SocialIcons from '../subComponents/SocialIcons';
-import PowerButton from '../subComponents/PowerButton';
-import ParticleComponent from '../subComponents/ParticleComponent';
-import BigTitle from '../subComponents/BigTitlte'
+import LogoComponent from "../subComponents/LogoComponent";
+import SocialIcons from "../subComponents/SocialIcons";
+import PowerButton from "../subComponents/PowerButton";
+import ParticleComponent from "../subComponents/ParticleComponent";
+import BigTitle from "../subComponents/BigTitlte";
 
 const Box = styled.div`
-background-color: ${props => props.theme.body};
-width: 100vw;
-height:100vh;
-position: relative;
-display: flex;
-justify-content: space-evenly;
-align-items: center;
-
-
-`
+  background-color: ${(props) => props.theme.body};
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+`;
 
 const Main = styled.div`
-border: 2px solid ${props => props.theme.text};
-color: ${props => props.theme.text};
-background-color: ${props => props.theme.body};
-padding: 2rem;
-width: 30vw;
-z-index:3;
-line-height: 1.5;
-cursor: pointer;
+  border: 2px solid ${(props) => props.theme.text};
+  color: ${(props) => props.theme.text};
+  background-color: ${(props) => props.theme.body};
+  padding: 2rem;
+  width: 30vw;
+  z-index: 3;
+  line-height: 1.5;
+  cursor: pointer;
 
-font-family: 'Ubuntu Mono',monospace;
-display: flex;
-flex-direction: column;
-justify-content: space-between;
+  font-family: "Ubuntu Mono", monospace;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 
-&:hover{
-    color: ${props => props.theme.body};
-    background-color: ${props => props.theme.text};
-}
-`
+  &:hover {
+    color: ${(props) => props.theme.body};
+    background-color: ${(props) => props.theme.text};
+  }
+`;
 
 const Title = styled.h2`
-display: flex;
-justify-content: center;
-align-items: center;
-font-size: calc(1em + 1vw);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: calc(1em + 1vw);
 
-${Main}:hover &{
-    &>*{
-        fill:${props => props.theme.body};
+  ${Main}:hover & {
+    & > * {
+      fill: ${(props) => props.theme.body};
     }
-}
+  }
 
-&>*:first-child{
-margin-right: 1rem;
-}
-`
+  & > *:first-child {
+    margin-right: 1rem;
+  }
+`;
 
 const Description = styled.div`
-color: ${props => props.theme.text};
-font-size: calc(0.6em + 1vw);
-padding: 0.5rem 0;
+  color: ${(props) => props.theme.text};
+  font-size: calc(0.6em + 1vw);
+  padding: 0.5rem 0;
 
+  ${Main}:hover & {
+    color: ${(props) => props.theme.body};
+  }
 
-${Main}:hover &{
-   
-        color:${props => props.theme.body};
-    
-}
-
-strong{
+  strong {
     text-transform: uppercase;
-}
-ul,p{
+  }
+  ul,
+  p {
     margin-left: 2rem;
-}
-`
+  }
+`;
 
 const MySkillsPage = () => {
-    return (
-        <ThemeProvider theme={lightTheme}>
-            <Box>
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <Box>
+        <LogoComponent theme="light" />
+        <SocialIcons theme="light" />
+        <PowerButton />
+        <ParticleComponent theme="light" />
+        <Main>
+          <Title>
+            <Design width={40} height={40} /> cyber security analyst
+          </Title>
 
-                <LogoComponent theme='light' />
-                <SocialIcons theme='light' />
-                <PowerButton />
-                <ParticleComponent theme='light' />
-                <Main>
-                    <Title>
-                        <Design width={40} height={40} /> cyber security analyst
-                    </Title>
-         
-                    <Description>
-                    Security Technology Expertise<br />
-Network Security Proficiency<br />
-Threat Intelligence Analysis<br />
-Incident Response Management<br />
-Risk Assessment and Management<br />
-Security Operations Center (SOC) Operations<br />
-Communication and Collaboration Skills<br />
-Continuous Learning and Professional Development
-                    </Description>
+          <Description>
+            Security Technology Expertise
+            <br />
+            Network Security Proficiency
+            <br />
+            Threat Intelligence Analysis
+            <br />
+            Incident Response Management
+            <br />
+            Risk Assessment and Management
+            <br />
+            Security Operations Center (SOC) Operations
+            <br />
+            Communication and Collaboration Skills
+            <br />
+            Continuous Learning and Professional Development
+          </Description>
+        </Main>
+        <Main>
+          <Title>
+            <Develope width={40} height={40} /> My Tools
+          </Title>
 
-                </Main>
-                <Main>
-                    <Title>
-                        <Develope width={40} height={40} /> My Tools 
-                    </Title>
-         
-                    <Description>
-                    Vulnerability Scanners<br/>
-Intrusion Detection and Prevention Systems<br/>
-Security Information and Event Management (SIEM) systems<br/>
-Malware Analysis Tools<br/>
-Penetration Testing Tools<br/>
-Firewall and Network Security Tools<br/>
-Forensic Analysis Tools<br/>
-Authentication and Identity Management Tools
-                    </Description>
+          <Description>
+            Vulnerability Scanners
+            <br />
+            Intrusion Detection and Prevention Systems
+            <br />
+            Security Information and Event Management (SIEM) systems
+            <br />
+            Malware Analysis Tools
+            <br />
+            Penetration Testing Tools
+            <br />
+            Firewall and Network Security Tools
+            <br />
+            Forensic Analysis Tools
+            <br />
+            Authentication and Identity Management Tools
+          </Description>
+        </Main>
 
-                </Main>
+        <BigTitle text="SKILLS" top="80%" right="30%" />
+      </Box>
+    </ThemeProvider>
+  );
+};
 
-                <BigTitle text="SKILLS" top="80%" right="30%" />
-
-            </Box>
-
-        </ThemeProvider>
-
-    )
-}
-
-export default MySkillsPage
+export default MySkillsPage;
